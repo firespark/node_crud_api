393477166a38c1adc4129b652260bea8368f3ab4
firespark <fireleit@gmail.com>
2024-10-04 10:55:10 +0800

    feat: add test data

---
+++ data.js
@@ -0,0 +1,8 @@
+const data = {
+    id: '1',
+    username: 'Petya',
+    age: 22,
+    hobbies: ['run', 'swim'],
+}
+
+export { data }
--- server.js
+++ server.js
@@ -3,45 +3,41 @@ import { getRequest } from './actions/get.js';
 import { postRequest } from './actions/post.js';
 import { putRequest } from './actions/put.js';
 import { deleteRequest } from './actions/delete.js';
+import { data } from '.data.js';
 
 const PORT = process.env.PORT || 4000
 
-//create our server object
 const server = http.createServer()
 
-// We define a function that runs in response to the request event
 server.on("request", (request, response) => {
-  // handle request based on method then URL
-  switch (request.method) {
-    case "GET":
-      switch (request.url) {
-        // response for unexpected get requests
+
+    request.users = data;
+
+    switch (request.method) {
+        case "GET":
+            getRequest(request, response)
+            break
+
+        case "POST":
+            postRequest(request, response)
+            break
+
+        case "PUT":
+            putRequest(request, response)
+            break
+
+        case "DELETE":
+            deleteRequest(request, response)
+            break
+
         default:
-          response.statusCode = 400
-          response.write(`CANNOT GET ${request.url}`)
-          response.end()
-      }
-      break
-
-    case "POST":
-      break
-
-    case "PUT":
-      break
-
-    case "DELETE":
-      break
-
-    default:
-      // Send response for requests with no other response
-      response.statusCode = 400
-      response.write("No Response")
-      response.end()
-  }
+            response.statusCode = 400
+            response.write("No Response")
+            response.end()
+    }
 })
 
-// get the server to start listening
+
 server.listen(PORT, err => {
-  // error checking
-  err ? console.error(err) : console.log(`listening on port ${PORT}`)
+    err ? console.error(err) : console.log(`listening on port ${PORT}`)
 })
