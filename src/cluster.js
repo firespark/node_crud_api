c29cf1ad3b806e453d2823d4b04769e6d618619d
firespark <fireleit@gmail.com>
2024-10-05 19:11:25 +0800

    refactor: change cluster.js

--- src/cluster.js
+++ src/cluster.js
@@ -2,30 +2,28 @@ import cluster from 'cluster';
 import http from 'http';
 import os from 'os';
 import { fork } from 'child_process';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const numCPUs = os.cpus().length;
-const BASE_PORT = 4000;
+const BASE_PORT = parseInt(process.env.PORT, 10);
 
-if (cluster.isMaster) {
+if (cluster.isPrimary) {
     console.log(`Master ${process.pid} is running`);
 
-    // Балансировщик нагрузки на порту 4000
     let currentWorker = 0;
     const workers = [];
 
     for (let i = 1; i < numCPUs; i++) {
-        // Создаем воркеры
         const worker = cluster.fork({ PORT: BASE_PORT + i });
         workers.push(worker);
     }
 
-    // Создаем сервер-балансировщик на порту 4000
     const loadBalancer = http.createServer((req, res) => {
-        // Round-robin распределение запросов по воркерам
         const workerPort = BASE_PORT + 1 + (currentWorker % (numCPUs - 1));
         currentWorker++;
 
-        // Проксируем запрос к воркеру
         const proxyReq = http.request(
             {
                 hostname: 'localhost',
@@ -48,7 +46,6 @@ if (cluster.isMaster) {
     });
 
 } else {
-    // Воркеры запускают приложение
     import('./server.js').then(() => {
         console.log(`Worker ${process.pid} started on port ${process.env.PORT}`);
     });
